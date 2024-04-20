de62f416e2334738c0a66470a1834f2ffaabf3ff
Caroline Mackin <cmackin712@gmail.com>
2024-04-20 00:55:06 -0400

    Initial Commit

---
+++ LitterBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LitterBox
+//
+//  Created by Caroline Mackin on 4/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LitterBox/LitterBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  LitterBoxApp.swift
+//  LitterBox
+//
+//  Created by Caroline Mackin on 4/20/24.
+//
+
+import SwiftUI
+
+@main
+struct LitterBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
